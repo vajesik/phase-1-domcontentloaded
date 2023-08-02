866db7f3511fd5613a2a71c7ace473a95254c554
Vanessa Jesik <vajesik@gmail.com>
2023-08-02 11:29:37 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+const element = document.getElementById("text");
+
+document.addEventListener("DOMContentLoaded", function () {
+  element.textContent = "This is really cool!";
+});
